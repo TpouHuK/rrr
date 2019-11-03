dd991d67f62d2725affc7cf5c2e93422e45abe0b
tpouhuk <tpouhuk@192.168.100.4>
2019-11-03 17:22:03 +0300

    road to stabilnost

--- values.lua
+++ values.lua
@@ -81,10 +81,10 @@ H.start = {
 }
 
 H.speed = {
-	degrees_sd = 20 + 10,
-	rotate_sd = 30 + 10,
-	macro_sd = 30 + 10,
-	degrees_sd = 30 + 10,
+	degrees_sd = 20, --+ 10,
+	rotate_sd = 30, --+ 10,
+	macro_sd = 30, --+ 10,
+	degrees_sd = 30, --+ 10,
 }
 
 
